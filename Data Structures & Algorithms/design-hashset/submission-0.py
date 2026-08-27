e2e3477d4e97d72e28e3a3be3a02f19fb58f9883
Muhammad Usman Mateen <74776275+usmanmateen@users.noreply.github.com>
2026-08-27 19:50:19 +0100

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,13 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.data = [False] * 1_000_001
+
+    def add(self, key: int) -> None:
+        self.data[key] = True
+
+    def remove(self, key: int) -> None:
+        self.data[key] = False
+
+    def contains(self, key: int) -> bool:
+        return self.data[key]
